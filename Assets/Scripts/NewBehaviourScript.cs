ff133952118363aeb3a231724336e1abdd8e0877
Wiash01 <aleev.1337@gmail.com>
2021-11-13 19:07:33 +0300

    двигание
    (но только горизонтальное)

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody2D rb;
+    private float HorizontalMove = 0f;
+    public float speed = 1f;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        HorizontalMove = Input.GetAxisRaw("Horizontal") * speed;
+
+    }
+    private void FixedUpdate() 
+    {
+        Vector2 targetVelocity = new Vector2(HorizontalMove * 10f, rb.velocity.y);
+        rb.velocity = targetVelocity;
     }
 }
